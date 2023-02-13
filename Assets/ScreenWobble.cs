8cc37d7179a6882ecfda1522539abfa29152791d
boggal <boggal1245@gmail.com>
2023-02-13 18:06:14 +1300

    sound

--- Assets/ScreenWobble.cs
+++ Assets/ScreenWobble.cs
@@ -2,7 +2,24 @@ using UnityEngine;
 
 public class ScreenWobble : MonoBehaviour
 {
-    public AnimationClip animationClip;
-    private Animation animationComponent;
+    public Animator animator;
+    
 
+    private void Start()
+    {
+        animator.enabled = false;  
+    }
+
+    private void Update()
+    {
+        if(Input.GetKey(KeyCode.W) || Input.GetKey(KeyCode.A) || Input.GetKey(KeyCode.S) || Input.GetKey(KeyCode.D)) 
+        {
+            animator.enabled = true;
+        }
+
+        if(Input.GetKeyUp(KeyCode.W) || Input.GetKeyUp(KeyCode.A) || Input.GetKeyUp(KeyCode.S) || Input.GetKeyUp(KeyCode.D)) 
+        {
+            animator.enabled = false;
+        }
+    }
 }
